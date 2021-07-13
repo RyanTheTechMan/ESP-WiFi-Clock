34216ab98c41a9afcf0efb00577e9348c34fd00b
Ryan Ernest <ryan@rivergroup.com>
2021-07-13 10:53:02 -0400

    Added a ton more
    Known Issue: SSID is invalid. Unknown solution :/

--- ESP_WiFi_Clock.ino
+++ ESP_WiFi_Clock.ino
@@ -16,6 +16,9 @@ unsigned long modeChangePinHoldTime = 0;
 unsigned long currentTime = 0;
 unsigned long oldTime = 0;
 unsigned long blinkInterval = 0;
+unsigned long lastBlink = 0;
+boolean isReady = false; //If the system is ready to run main loop
+boolean triedConnectingToWifi = false;
 
 // — Global Parameters —
 
@@ -24,6 +27,8 @@ ESP8266WebServer server(80); //Create instance of WebServer on port 80
 WiFiManager wifiManager; //Create isntance of WiFiManager
 WiFiUDP ntpUDP; //Opens a UDP Port
 NTPClient timeClient(ntpUDP, "pool.ntp.org", -18000); //Starts a NTPClient to get the time. //TODO: replace number and add/subtract based on timezone & DST
+char* ESPWifiSSID;
+String esp_ssid;
 
 // — System Modes —
 const short MODE_NORMAL     = 0;
@@ -35,65 +40,59 @@ const short EEPROM_Mode = 0;
 
 void setup() {
   Serial.begin(115200); //Opens serial connection for debugging
+  delay(1000);
   Serial.println();
+  
+  // — Set SSID —
+  String mac = String(WiFi.macAddress());
+  mac.replace(":", "");
+  esp_ssid = ("ESP Clock " + mac.substring(6)); //Idk why this doesnt work
+  
+  int ssid_len = esp_ssid.length()+1;
+  char essid[ssid_len];
+  esp_ssid.toCharArray(essid, ssid_len);
+  ESPWifiSSID = essid;
+
   // — Setup EEPROM —
-  //EEPROM.begin(2);
-  //currentMode = EEPROM.read(EEPROM_Mode);
+  EEPROM.begin(512);
+  currentMode = EEPROM.read(EEPROM_Mode);
 
   // — Setup Pins —
   pinMode(MODE_PIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-
   WiFi.mode(WIFI_STA); //Sets WiFi Mode to STA+AP
 
-  if (currentMode == MODE_NORMAL) {
-    Serial.println(">>> Starting in mode: NORMAL");
-    while (!ConnectToWiFi(wifiManager.getWiFiSSID(), wifiManager.getWiFiPass())){} //Keep trying to connect to network. MOVE TO LOOP TO STOP LAG
-    
-    LEDOn();
-    LEDOffIn(2000);
-    Serial.println();
-    Serial.print("Got IP: ");  Serial.println(WiFi.localIP()); //Print IP that router gave client
-    timeClient.begin(); //Start keeping track of time
-    Serial.println("Time Client Started!");
-  }
+  /*Serial.println("WL_CONNECTED" + String(WL_CONNECTED));
+    Serial.println("WL_NO_SHIELD" + String(WL_NO_SHIELD));
+    Serial.println("WL_IDLE_STATUS" + String(WL_IDLE_STATUS));
+    Serial.println("WL_NO_SSID_AVAIL" + String(WL_NO_SSID_AVAIL));
+    Serial.println("WL_SCAN_COMPLETED" + String(WL_SCAN_COMPLETED));
+    Serial.println("WL_CONNECT_FAILED" + String(WL_CONNECT_FAILED));
+    Serial.println("WL_CONNECTION_LOST" + String(WL_CONNECTION_LOST));
+    Serial.println("WL_DISCONNECTED" + String(WL_DISCONNECTED));
+  */
 
-  if (currentMode == MODE_SETTINGS) {
-    Serial.println(">>> Starting in mode: SETTINGS");
-    Serial.print("Starting HTTP Server");
-    WiFi.softAP("ESP Clock - Settings"); //Starts hosted directly on ESP
+  //wifiManager.autoConnect(ESPWifiSSID);
 
-    delay(100); //A small pause to allow WiFi chip to fully start
-
-    server.on("/", handle_OnConnect);
-    server.onNotFound(handle_NotFound);
-
-    server.begin(); //Run host and allow clients to view website
-    Serial.println("HTTP Server Started!");
-
-    if (MDNS.begin("clock")) { //Start the mDNS responder for clock.local
-      Serial.println("mDNS responder started");
-      MDNS.addService("http", "tcp", 80);
-    } else {
-      Serial.println("Error setting up MDNS responder! Connect using: ");
-      Serial.print(WiFi.localIP());
-    }
-  } //else WiFi.begin(ssid, password); //Shows website on connected network //MOST LIKELY NOT NEEDED!!
-
+  /*WiFi.begin(wifiManager.getWiFiSSID(), wifiManager.getWiFiPass());
+    Serial.println("Connecting to WIFI: " + wifiManager.getWiFiSSID() + "\nWith password: " + wifiManager.getWiFiPass());
+    while (true){
+    Serial.println(WiFi.status());
+    delay(100);
+    }*/
 }
 
 void loop() {
-  if(blinkInterval != 0 && (millis() - oldTime >= blinkInterval)) {
+  if (blinkInterval != 0 && (millis() - lastBlink >= blinkInterval)) {
     LEDToggle();
+    lastBlink = millis();
   }
-  
+
   // — Delta Time — Time since the last loop
   oldTime = currentTime;
   currentTime = millis();
   const unsigned long deltaTime = currentTime - oldTime;
-  
-  
 
   if (turnOffLightTime != 0 && millis() > turnOffLightTime) {
     LEDOff();
@@ -109,14 +108,102 @@ void loop() {
     if (modeChangePinHoldTime > 5000) {
       modeChangePinHoldTime = 0;
       blinkInterval = 0;
+      lastBlink = millis(); //make an easier startBlink() function
       wifiManager.resetSettings();
       Serial.println("WiFi Settings have been reset!");
-      
+
       Serial.println("Restarting!");
       Serial.println("Restarting!");
       Serial.println("Restarting!");
       ESP.restart();
     }
+    else if (modeChangePinHoldTime > 1000) {
+      modeChangePinHoldTime = 0;
+      Serial.println("Set new EEPROM!");
+      EEPROM.write(EEPROM_Mode, 2);
+      EEPROM.commit();
+    }
+    else if (modeChangePinHoldTime > 200) {
+      modeChangePinHoldTime = 0;
+      Serial.print("Current Mode in Code: "); Serial.println(currentMode);
+      Serial.print("Current Mode in EEPROM: "); Serial.println(EEPROM.read(EEPROM_Mode));
+    }
+  }
+
+  if (!isReady) {
+    if (currentMode == MODE_NORMAL) {
+      //Serial.println(">>> Starting in mode: NORMAL");
+
+      if (wifiManager.getWiFiSSID() == "") {
+        Serial.println("Network SSID is INVALID. Switching to setup mode!");
+        delay(2000);
+        EEPROM.write(EEPROM_Mode, MODE_WIFI_SETUP);
+        delay(10000);
+      }
+      else if (WiFi.status() == WL_IDLE_STATUS) {
+        if (triedConnectingToWifi) {
+          Serial.println("I guess the network was not found????");
+          delay(10000);
+        }
+        else {
+          Serial.println("Starting to connect to network: " + wifiManager.getWiFiSSID());
+          WiFi.begin(wifiManager.getWiFiSSID(), wifiManager.getWiFiPass());
+          triedConnectingToWifi = true;
+          delay(500); //Wait a moment to allow WiFi Module to start connecting
+        }
+      }
+      else if (WiFi.status() == WL_CONNECTED) {
+        LEDOn();
+        LEDOffIn(5000);
+        Serial.println();
+        Serial.print("Got IP: ");  Serial.println(WiFi.localIP()); //Print IP that router gave client
+        timeClient.begin(); //Start keeping track of time
+        Serial.println("Time Client Started!");
+        isReady = true;
+      }
+      else if (WiFi.status() == WL_DISCONNECTED) {
+        Serial.println("Failed to connect???");
+      }
+    }
+    else if (currentMode == MODE_SETTINGS) {
+      //Serial.println(">>> Starting in mode: SETTINGS");
+      Serial.print("Starting HTTP Server");
+      WiFi.softAP("ESP Clock - Settings"); //Starts hosted directly on ESP
+
+      delay(100); //A small pause to allow WiFi chip to fully start
+
+      server.on("/", handle_OnConnect);
+      server.onNotFound(handle_NotFound);
+
+      server.begin(); //Run host and allow clients to view website
+      Serial.println("HTTP Server Started!");
+
+      if (MDNS.begin("clock")) { //Start the mDNS responder for clock.local
+        Serial.println("mDNS responder started");
+        MDNS.addService("http", "tcp", 80);
+      } else {
+        Serial.println("Error setting up MDNS responder! Connect using: ");
+        Serial.print(WiFi.localIP());
+      }
+
+      isReady = true;
+    } //else WiFi.begin(ssid, password); //Shows website on connected network //MOST LIKELY NOT NEEDED!!
+    else if (currentMode == MODE_WIFI_SETUP) {
+      if (!wifiManager.process()){
+        Serial.println("Starting portal");
+        wifiManager.startConfigPortal(ESPWifiSSID);
+      }
+      else {
+        Serial.println("Running... Waiting for finish.");
+      }
+      
+      //wifiManager.startWebPortal();
+      //wifiManager.autoConnect(ESPWifiSSID);
+    }
+  }
+  else {
+    Serial.println("Everything is ready!!!");
+    delay(10000);
   }
 }
 
@@ -156,8 +243,6 @@ String SendHTML() {
   return doc;
 }
 
-//run EEPROM.commit() to save changes
-
 void LEDToggle() {
   digitalWrite(LED_BUILTIN,  !digitalRead(LED_BUILTIN));
 }
@@ -172,28 +257,4 @@ void LEDOn() {
 
 void LEDOff() {
   digitalWrite(LED_BUILTIN,  LOW);
-}
-
-boolean ConnectToWiFi(String[] ssid, String[] pass) {
-  Serial.println("Connecting to WIFI: " + ssid + "\nWith password: " + pass);
-  WiFi.begin(ssid, pass);
-
-  int wifiTimeout = 15000;
-  boolean cnt = true; //Connected to WiFi Network
-  while (WiFi.status() != WL_CONNECTED) { //Wait until connected to network
-    if (wifiTimeout <= 0) {
-      cnt = false;
-      break;
-    }
-    wifiTimeout -= 500;
-    Serial.print(".");
-    LEDToggle();
-    delay(500);
-  }
-
-  if (!cnt) { //Go into a loop and try connecting again
-    WiFi.disconnect()
-    return false;
-  }
-  return true;
 }
